45f4f97b68932b8bef4f28a1cc7d0ec29b7548a8
vitorosenbergre <vitorosenbergre@gmail.com>
2025-07-13 20:32:44 -0300

    [feature] full responsiviness application

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -25,7 +25,7 @@ const Navbar: React.FC = () => {
       <div className="container">
         <a className="navbar-brand" href="#" onClick={(e) => { e.preventDefault(); console.log('Clicked: Home (Brand)'); handleNavigation('home'); }} style={{ cursor: 'pointer' }}>Engenheiro Álvaro Argôlo</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"><i className="bi bi-list"></i></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
